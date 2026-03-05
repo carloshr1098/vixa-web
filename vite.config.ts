d4ce090700ec6f19915f2a0cb8e1579a49256783
Carlos Hernandez <carloshr1098@gmail.com>
2026-03-05 15:56:31 -0600

    upd: actualización de número de WhatsApp

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/vixa-web/', // <-- ESTA ES LA LÍNEA CLAVE
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
